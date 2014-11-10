d97e42c765aa343a13dcf4c04b99ed4711171cf4
JoshuaChing <joshua-ching@hotmail.com>
2014-11-09 23:22:29 -0500

    automatic clock update

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,10 @@
-var time = moment().format('h:mm a');
-var date = moment().format('dddd MMM Do');
+function updateClock(){
+	var time = moment().format('h:mm a');
+	var date = moment().format('dddd MMM Do');
+	$('#time').text(time);
+	$('#date').text(date);
 
-$('#time').text(time);
-$('#date').text(date);
+	setTimeout(updateClock,1000);
+}
+
+updateClock();
